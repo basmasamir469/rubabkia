0e0d06c94bdb1ffaeaedb94b2294919fb3994fe5
basmasamir469 <basmaelazony@gmail.com>
2022-03-14 23:09:59 +0200

    deplllooyy

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/Rubabika-main'));
+app.use(express.static(__dirname+'/dist'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Rubabkia-main/'}),
+    res.sendFile(path.join(__dirname+'/dist/index.html')),
 );
 
 // Start the app by listening on the default Heroku port
